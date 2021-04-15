be934c315cfb0c0db3b0199e2159117028ce9150
Admin <Admin@DESKTOP-HGVC72S>
2021-04-15 18:26:59 -0500

    Uso de paquetes y radio button

---
+++ src/Operaciones/OSuma.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+
+/**
+ *
+ * @author Admin
+ */
+public class OSuma {
+    public int sumar (int x, int y){
+        return (x+y);
+        
+       
+    }
+}
